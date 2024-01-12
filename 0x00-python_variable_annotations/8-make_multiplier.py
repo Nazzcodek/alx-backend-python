907450814c023fbe5378b910973ff2150a86d0f5
Nazzcodek <bellnass09@gmail.com>
2024-01-12 19:16:47 +0000

    pycodestle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,7 +5,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """it takes a float and multiply"""
- 
+
     def multiply(fun: float) -> float:
         """fun function"""
         return fun * multiplier
