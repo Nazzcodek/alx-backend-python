dc1f9dd282c78b5233379f50fbb8861b2126635c
Nazzcodek <bellnass09@gmail.com>
2024-01-12 17:18:42 +0000

    task 0 and 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this is a function module for addition using type annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """this function is the addition of two float number"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+this is a string concatination function module
+"""
+
+
+def concat(str1: string, str2: string) -> string:
+    """this function concat two string and
+       returned annoted string
+    """
+    return str1 + str2
