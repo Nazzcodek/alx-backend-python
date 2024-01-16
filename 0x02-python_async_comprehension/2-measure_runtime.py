473d4e4479f1b1c30e7d8033a82b1f9de1d28347
Nazzcodek <bellnass09@gmail.com>
2024-01-16 11:37:21 +0000

    task 2 again

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """this methode mesaures the runtime of async comp"""
     start_time = time.perf_counter()
-    asyncio.gather(*[async_comprehension() for _ in range(4)])
+    try:
+        await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    except asyncio.CancelledError:
+        pass
     total_time = time.perf_counter() - start_time
     return total_time
