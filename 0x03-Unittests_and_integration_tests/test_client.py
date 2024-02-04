63941b9845c6fc363ca2b51f8a762d996a3acb05
Nazzcodek <bellnas09@gmail.com>
2024-02-04 10:50:18 +0000

    client test 1

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''this is the client test module'''
+import unittest
+import json
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized, parameterized_class
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class for Testing Github Org Client """
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+    def test_public_repos_url(self):
+        """ Test that the result of _public_repos_url is the expected one
+        based on the mocked payload
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
