f8aab8d139eaa6fac204d8fedb7044e3bef8f4f4
Nazzcodek <bellnas09@gmail.com>
2025-06-02 07:47:12 +0100

    testing client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """Test suite for GithubOrgClient."""
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
@@ -28,7 +32,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     def test_public_repos_url(self):
         """
-        Test that _public_repos_url returns the correct 
+        Test that _public_repos_url returns the correct
         repos_url from org payload.
         """
         payload = {"repos_url": "https://api.github.com/orgs/testorg/repos"}
